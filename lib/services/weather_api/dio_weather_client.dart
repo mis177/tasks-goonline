dc87077cd64439d2b91a41a024f495d225ded248
MichalJ <michal.jamroz@t2t-system.com>
2024-08-02 15:01:03 +0200

    CHANGED dio weather request

--- lib/services/weather_api/dio_weather_client.dart
+++ lib/services/weather_api/dio_weather_client.dart
@@ -2,24 +2,44 @@ import 'package:dio/dio.dart';
 import 'package:notes_goonline/models/weather_model.dart';
 
 class DioWeatherClient {
-  final Dio _dio = Dio();
+  final Dio _dio;
   final _baseUrl = "http://api.weatherapi.com/v1";
 
   static final DioWeatherClient _instance = DioWeatherClient._internal();
-  DioWeatherClient._internal();
+  DioWeatherClient._internal() : _dio = Dio() {
+    _configureDio();
+  }
+
   factory DioWeatherClient() => _instance;
 
-  Future<Weather?> getWeather(
-      {required String key, required String location}) async {
-    Weather? weather;
-    try {
-      Response weatherData =
-          await _dio.get('$_baseUrl/current.json?key=$key&q=$location');
+  void _configureDio() {
+    _dio.options
+      ..baseUrl = _baseUrl
+      ..connectTimeout = const Duration(milliseconds: 5000) // 5 seconds
+      ..receiveTimeout = const Duration(milliseconds: 3000); // 3 seconds
+  }
 
-      weather = Weather.fromJson(weatherData.data);
-    } on DioException {
-      throw DioException;
+  Future<Weather?> getWeather({required String key, required String location}) async {
+    try {
+      final Response response = await _dio.get(
+        '/current.json',
+        queryParameters: {
+          'key': key,
+          'q': location,
+        },
+      );
+
+      if (response.statusCode == 200) {
+        return Weather.fromJson(response.data);
+      } else {
+        throw Exception('Failed to load weather data');
+      }
+    } on DioException catch (e) {
+      // Handle specific DioException cases if needed
+      throw Exception('DioException: ${e.message}');
+    } catch (e) {
+      // Handle general exceptions
+      throw Exception('Unexpected error: $e');
     }
-    return weather;
   }
 }
